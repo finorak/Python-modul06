c4b94978ea474f7af5c94bc6c19c4483d9fb98c9
finorak <finoana265@gmail.com>
2026-03-19 08:07:34 +0300

    removed uncessary in __init__.py

--- alchemy/__init__.py
+++ alchemy/__init__.py
@@ -3,9 +3,8 @@ __author__ = "Master Pythonnicius"
 
 from typing import Any
 from .elements import create_water, create_fire
-from .potions import strength_potion
 
-__all__ = ["create_water", "create_fire", "strength_potion"]
+__all__ = ["create_water", "create_fire"]
 
 
 def __getattr__(name: str) -> Any:
